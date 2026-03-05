3cc612af1c217221586bd000c266cd68ebdeff1f
Muhammad Abdullah <abdullah.webdev9176@gmail.com>
2026-03-05 16:45:53 +0500

    updated filter

--- public/assets/js/filters.js
+++ public/assets/js/filters.js
@@ -1,43 +1,29 @@
-let selectedCondition = 'All';
-
+let selectedCondition = ['All'];
 
 function handleConditionClick(e) {
-    e.preventDefault();
+    const clickedElement = $(e.target);
+    const clickedValue = clickedElement.val();
+    const items = $('.condition-item');
+    const all = $('#condition-all');
 
-    const clicked = e.target.value;
-    let items = $('.condition-item');
+    if (clickedValue === 'All') {
 
-    if(clicked === 'all') {
-        selectedCondition = 'All';
         items.prop('checked', false);
-    }else{
-        const anyChecked = items.toArray().some(item => item.checked);
-        $('#condition-all').prop('checked', !anyChecked);
+        all.prop('checked', true);
+        selectedCondition = ['All'];
+    } else {
+
+        all.prop('checked', false);
+
+        selectedCondition = items.filter(':checked').map(function() {
+            return this.value;
+        }).get();
+
+        if (selectedCondition.length === 0) {
+            selectedCondition = ['All'];
+            all.prop('checked', true);
+        }
     }
 
+    console.log(selectedCondition);
 }
-
-// function handleConditionClick(e) {
-
-//     const clicked = e.target.value;
-
-//     const all = document.getElementById('condition-all');
-//     const items = document.querySelectorAll('.condition-item');
-
-//     const itemsArray = [...items];
-
-//     if (clicked === 'all') {
-//         itemsArray.forEach(item => item.checked = false);
-//     } else {
-//         const anyChecked = itemsArray.some(item => item.checked);
-//         all.checked = !anyChecked;
-//     }
-
-//     const checkedItems = itemsArray.filter((i) => {
-//         return i.checked;
-//     })
-
-//     checkedItemsValues = checkedItems.map((i) => {
-//         return i.value;
-//     })
-// }
