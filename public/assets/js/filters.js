b4db4b97ef9190cb78357e6df4d4071ea049a322
Muhammad Abdullah <abdullah.webdev9176@gmail.com>
2026-03-06 17:36:11 +0500

    updated filter

--- public/assets/js/filters.js
+++ public/assets/js/filters.js
@@ -3,6 +3,7 @@ let selectedBrand = [];
 let selectedSeries = [];
 let selectedModel = [];
 let selectedLengthRange = { min: 0, max: 100 };
+let selectedYearRange = { min: 0, max: 100 };
 
 function handleConditionClick(e) {
     const clickedElement = $(e.target);
@@ -69,10 +70,11 @@ let minLength = $("#minVal").data("minlength") || 0;
 let maxLength = $("#maxVal").data("maxlength") || 100;
 let minYear = $("#minYearVal").data("minyear") || 0;
 let maxYear = $("#maxYearVal").data("maxyear") || 100;
-console.log("Min Length:", minLength);
-console.log("Max Length:", maxLength);
-console.log("Min Year:", minYear);
-console.log("Max Year:", maxYear);
+
+// console.log("Min Length:", minLength);
+// console.log("Max Length:", maxLength);
+// console.log("Min Year:", minYear);
+// console.log("Max Year:", maxYear);
 
 $("#rangeSlider").slider({
     range: true,
@@ -84,6 +86,12 @@ $("#rangeSlider").slider({
     slide: function (event, ui) {
         $("#minVal").text(ui.values[0]);
         $("#maxVal").text(ui.values[1]);
+
+        selectedLengthRange = {
+            min: ui.values[0],
+            max: ui.values[1]
+        }
+
     }
 });
 
@@ -97,5 +105,12 @@ $("#yearRangeSlider").slider({
     slide: function (event, ui) {
         $("#minYearVal").text(ui.values[0]);
         $("#maxYearVal").text(ui.values[1]);
+
+        console.log('Year Range:', ui.values[0], ui.values[1]);
+
+        selectedYearRange = {
+            min: ui.values[0],
+            max: ui.values[1]
+        }
     }
 });
